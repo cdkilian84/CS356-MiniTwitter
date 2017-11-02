1f2dcdf50d10c56f7f86dbe0ca47c06372720e7d
Christopher Kilian <cdkilian@gmail.com>
2017-11-02 15:03:36 -0700

    Another test

--- src/cs356/minitwitter/Driver.java
+++ src/cs356/minitwitter/Driver.java
@@ -15,7 +15,7 @@ public class Driver {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Just testing
+        // Another test woot woot
     }
     
 }
