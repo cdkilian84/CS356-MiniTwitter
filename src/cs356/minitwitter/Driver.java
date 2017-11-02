38ab0aac4e0a6625ed767e395848e019f41d602c
Christopher Kilian <cdkilian@gmail.com>
2017-11-02 15:08:02 -0700

    Stupid testing

--- src/cs356/minitwitter/Driver.java
+++ src/cs356/minitwitter/Driver.java
@@ -15,7 +15,7 @@ public class Driver {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Another test woot woot
+        // Stupid github
     }
     
 }
